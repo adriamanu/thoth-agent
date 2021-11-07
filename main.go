a91779ee32a4377fbf9ab5b3dbc2437eae40cbc9
adriamanu <emmanuel.adrian54@gmail.com>
2021-11-07 22:12:05 +0100

    server

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	cpu "github.com/adriamanu/thoth-agent/cpu"
-	disk "github.com/adriamanu/thoth-agent/disk"
-	memory "github.com/adriamanu/thoth-agent/memory"
+	"github.com/adriamanu/thoth-agent/server"
 )
 
 func main() {
-	for {
-		memory := memory.Stat()
-		fmt.Printf("Memory percentage used: %.3f\n", memory.UsedMemoryPercentage)
-
-		cpu := cpu.Stat()
-		fmt.Printf("CPU percentage used: %.3f\n", cpu.UsedPercentage)
-
-		disk := disk.Stat()
-		fmt.Printf("Disk percentage used: %.3f\n\n", disk.UsedSpacePercentage)
-
-		// time.Sleep(1 * time.Second)
-		time.Sleep(200 * time.Millisecond)
-	}
+	server.Server()
 }
---
+++ server/server.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/adriamanu/thoth-agent/cpu"
+	"github.com/adriamanu/thoth-agent/disk"
+	"github.com/adriamanu/thoth-agent/memory"
+)
+
+type healthStruct struct {
+	Status string `json:"status"`
+	Date   string `json:"date"`
+}
+
+func healthCheckHandler(res http.ResponseWriter, req *http.Request) {
+	healthAsJson, err := json.Marshal(healthStruct{Status: "alive", Date: time.Now().String()})
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.WriteHeader(200)
+	res.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(res, "%s", healthAsJson)
+}
+
+type resourceUsage struct {
+	Cpu cpu.CPUStat `json:"cpu"`
+	Memory memory.MemoryStat `json:"memory"`
+	Disk disk.DiskStat `json:"disk"`
+}
+
+
+func resourceUsageHandler(res http.ResponseWriter, req *http.Request) {
+	c := cpu.Stat()
+	m := memory.Stat()
+	d := disk.Stat()
+	ru, err := json.Marshal(resourceUsage{c,m, d})
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.WriteHeader(200)
+	res.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(res, "%s", ru)
+}
+
+func visualizationHandler(res http.ResponseWriter, req *http.Request) {
+http.ServeFile(res, req, "./server/chart.html")
+}
+
+func Server() {
+	http.HandleFunc("/", healthCheckHandler)
+	http.HandleFunc("/resource-usage", resourceUsageHandler)
+	http.HandleFunc("/visualization", visualizationHandler)
+
+	log.Fatal(http.ListenAndServe(":11805", nil))
+}
